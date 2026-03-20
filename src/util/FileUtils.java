5f1635f0cc4feda09d480bf5588879d3bd7ea7e9
H. S. Nam Bình <nahmsix2005@gmail.com>
2026-03-20 23:09:37 +0700

    feat: simplify file reading with UTF-8 encoding

--- src/util/FileUtils.java
+++ src/util/FileUtils.java
@@ -1,10 +1,11 @@
 package util;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
 
 public class FileUtils {
     public static String readFile(String path) throws IOException {
-        return new String(Files.readAllBytes(Paths.get(path)));
+        return Files.readString(Paths.get(path), StandardCharsets.UTF_8);
     }
 }
