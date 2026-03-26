a8c55675263a44dc37d89ea0beb51d7f01bb4e01
H. S. Nam Bình <nahmsix2005@gmail.com>
2026-03-26 22:59:02 +0700

    feat: return exit code from command handler

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import cli.CommandHandler;
 public class Main {
     public static void main(String[] args) throws Exception {
         CommandHandler handler = new CommandHandler();
-        handler.handle(args);
+        int exitCode = handler.handle(args);
+        System.exit(exitCode);
     }
 }
